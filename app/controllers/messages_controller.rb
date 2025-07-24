8c6d68f40073394f3cfbbc917e3ae1ead1c97b85
Josh Frank <josh_frank@icloud.com>
2025-07-24 19:53:33 -0400

    adding messages#create route

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,13 @@
 class MessagesController < ApplicationController
 
   def create
-    render json: { test: 'Test' }, status: :ok
+    @new_message = Message.new( message_params )
+    @send_message = MessagesHelper.send( message_params[ :to ], message_params[ :content ] )
+    if @new_message.save && !@send_message.error
+      render json: @new_message, status: 200
+    else
+      render json: @new_message.errors.full_messages, status: 400
+    ende
   end
 
   private
