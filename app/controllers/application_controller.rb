2a4a986166550bf36f032b312dbc41117164164c
Josh Frank <josh_frank@icloud.com>
2025-07-24 20:59:59 -0400

    adding application#about route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  def about
+    render json: { messages: [ "Hypnosis API v#{ ENV[ 'APP_VERSION' ] } - ©#{ Date.today.year } Hypnotic Power LLC" ] }, status: 200
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "application#about", as: "about"
 
   get "messages", to: "messages#show"
   post "messages", to: "messages#create"
